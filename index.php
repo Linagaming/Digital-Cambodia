5118192d2240d179a719d8e4f0345dcb052e68e4
Voeurn Lina <88647240+Linagaming@users.noreply.github.com>
2025-02-04 00:02:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,332 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!---------------------------------------------------------------- Icon --------------------------------------------------->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!---------------------------------------------------------------- Icon --------------------------------------------------->
+
+    <!------------------------------------------------------------ Google Font ------------------------------------------------>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Suwannaphum:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Preahvihear&display=swap" rel="stylesheet">
+    <!------------------------------------------------------------ Google Font ----------------------------------------------->
+
+    <!------------------------------------------------------------- Bootstrap ------------------------------------------------>
+    <link rel="stylesheet" href="boostrap/bootstrap.min.css">
+    <!------------------------------------------------------------- Bootstrap ------------------------------------------------>
+    <!-- AOS Animation -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+
+
+    <link rel="stylesheet" href="css/Style.css">
+    <link rel="icon" type="image/x-icon" href="icon/digitalization (1).png">
+    <title>Digital Cambodia</title>
+</head>
+<?php include 'My PHP/header.php'; ?>
+
+
+
+
+<!-- Content Slide -->
+<div class="container-fluid content-slide">
+    <div class="row">
+        <div class="box-slide col-12 col-md-12 col-sm-12 col col-lg-12 col-xl-12" data-aos="fade-up" data-aos-once="true">
+            <div class="slide">
+                <div class="slide-img">
+                    <img src="img/pexels-mikael-blomkvist-6476584.jpg" alt="">
+                    <div class="slide-text">
+                        <div class="manage-text-slide" data-aos="fade-up" data-aos-once="true">
+                            <span>
+                                ឌីជីថលកម្ពុជា គឺជាគេហទំព័រមួយដែលទាន់សម័យបំផុត និងមានភាពងាយស្រួលក្នុងការតាមដាន ព័ត៌មានថ្មីៗប្រចាំថ្ងៃ។
+                            </span>
+                            <div class="btn-read-more">
+                                <a href="#ai"><span>ព័ត៌មានថ្មីៗ</span></a>
+                                <a href="#"><span>អ្នកនិពន្ធ</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="shadow-img-slide"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Manage Content -->
+<div class="container-fluid manage-content">
+    <div class="row">
+        <div class="content-center col-12 col-md-12 col-sm-12 col col-lg-12 col-xl-12">
+            <div class="title-content-category" data-aos="fade-up">
+                <p id="ai">ព័ត៌មានថ្មីៗអំពី <strong>AI</strong></p>
+            </div>
+            <a href="detail.php" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/photo_2025-01-29_22-01-12.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 29 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span id="title">
+                                    បច្ចុប្បន្នភាពចុងក្រោយបង្ហាញថា DeepSeek បានបង្កើនការប្រកួតប្រជែងយ៉ាងខ្លាំងជាមួយ OpenAI នៅក្នុងវិស័យបច្ចេកវិទ្យា
+                                </span>
+                                <span>
+                                    បច្ចុប្បន្នភាពចុងក្រោយបង្ហាញថា #DeepSeek បានបង្កើនការប្រកួតប្រជែងយ៉ាងខ្លាំងជាមួយ OpenAI នៅក្នុងវិស័យបច្ចេកវិទ្យា AI។ DeepSeek បានបង្ហាញម៉ូដែល AI ថ្មីដែលមានសមត្ថភាពខ្ពស់ ដោយចំណាយតិចជាង #OpenAI និងក្រុមហ៊ុនអាមេរិកធំៗ។ ការរីកចម្រើននេះបាននាំឱ្យតម្លៃទីផ្សាររបស់ក្រុមហ៊ុនបច្ចេកវិទ្យាធំៗដូចជា Nvidia, Microsoft, Meta, និង Alphabet ធ្លាក់ចុះយ៉ាងខ្លាំង។
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/photo_2025-01-30_09-31-25.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 30 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span>
+                                    DeepSeek បានប្រើបច្ចេកទេស “mixture of experts” ដែលអនុញ្ញាតឱ្យប្រើប្រាស់ធនធានកុំព្យូទ័រដែលចាំបាច់សម្រាប់កិច្ចការតែមួយចំនួនប៉ុណ្ណោះ ដោយធ្វើឱ្យប្រសិទ្ធភាពកាន់តែខ្ពស់។ ម៉ូដែល AI របស់ពួកគេត្រូវបានបង្កើតដោយចំណាយត្រឹមតែ ៦ លានដុល្លារ ប្រៀបធៀបនឹងការចំណាយរាប់រយលានដុល្លាររបស់ក្រុមហ៊ុនអាមេរិក។
+                                </span>
+                                <span>
+                                    DeepSeek បានប្រើបច្ចេកទេស “mixture of experts” ដែលអនុញ្ញាតឱ្យប្រើប្រាស់ធនធានកុំព្យូទ័រដែលចាំបាច់សម្រាប់កិច្ចការតែមួយចំនួនប៉ុណ្ណោះ ដោយធ្វើឱ្យប្រសិទ្ធភាពកាន់តែខ្ពស់។ ម៉ូដែល AI របស់ពួកគេត្រូវបានបង្កើតដោយចំណាយត្រឹមតែ ៦ លានដុល្លារ ប្រៀបធៀបនឹងការចំណាយរាប់រយលានដុល្លាររបស់ក្រុមហ៊ុនអាមេរិក។
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/photo_2025-01-29_11-46-43.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 31 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span>
+                                    DeepSeek ត្រូវបានបង្កើតឡើងក្នុងឆ្នាំ 2023 ដោយលោក Liang Wenfeng ហើយម៉ូឌែលជំនាន់ក្រោយបំផុតរបស់ក្រុមហ៊ុនគឺជា DeepSeek-V3 និងមានល្បឿនដំណើរការនៃប្រព័ន្ធ AI ឈានមុខដូចគ្នាទៅនឹង ChatGPT របស់ OpenAI ផងដែរ។ ស្របនឹងពេលដែល OpenAI បានចំណាយថវិកាអស់ជាងខ្ទង់ពាន់លានដុល្លារ ដើម្បីបង្កើត ChatGPT ជាច្រើនជំនាន់មក ដោយឡែក DeepSeek ចំណាយត្រឹមតែក្រោម 6 លានដុល្លារប៉ុណ្ណោះ នេះបើយោងតាមរបាយការណ៍ដាក់បង្ហាញមកថ្មីៗនេះ។
+                                </span>
+                                <span>
+                                    DeepSeek ត្រូវបានបង្កើតឡើងក្នុងឆ្នាំ 2023 ដោយលោក Liang Wenfeng ហើយម៉ូឌែលជំនាន់ក្រោយបំផុតរបស់ក្រុមហ៊ុនគឺជា DeepSeek-V3 និងមានល្បឿនដំណើរការនៃប្រព័ន្ធ AI ឈានមុខដូចគ្នាទៅនឹង ChatGPT របស់ OpenAI ផងដែរ។ ស្របនឹងពេលដែល OpenAI បានចំណាយថវិកាអស់ជាងខ្ទង់ពាន់លានដុល្លារ ដើម្បីបង្កើត ChatGPT ជាច្រើនជំនាន់មក ដោយឡែក DeepSeek ចំណាយត្រឹមតែក្រោម 6 លានដុល្លារប៉ុណ្ណោះ នេះបើយោងតាមរបាយការណ៍ដាក់បង្ហាញមកថ្មីៗនេះ។
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/Deepseek-header.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 30 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span>
+                                    DeepSeek ត្រូវរងការវាយប្រហារធំ បណ្តាលឱ្យបញ្ឈប់ការចុះឈ្មោះអ្នកថ្មីបណ្តោះអាសន្ន
+                                </span>
+                                <span>
+                                    DeepSeek AI, ដែលជាម៉ូដែល AI របស់ប្រទេសចិន ដែលបានបង្ហាញសមត្ថភាពខ្លាំងចាប់តាំងពីការបញ្ចេញ Model V3 នៅចុងឆ្នាំ 2024, បានក្លាយជាការចាប់អារម្មណ៍យ៉ាងខ្លាំង។ វាត្រូវបានដាក់ចំណាត់ថ្នាក់លេខ ១ នៅលើ App Store និង Play Store ដោយអាចលើស ChatGPT ក្នុងរយៈពេលខ្លី។
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="content-center col-12 col-md-12 col-sm-12 col col-lg-12 col-xl-12">
+            <div class="title-content-category" data-aos="fade-up" data-aos-once="true">
+                <p>ព័ត៌មានអំពី <strong>ឌីជីថលកម្ពុជា</strong></p>
+            </div>
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/Digital Cambodia/អ្នកធ្វើការជំនាញផ្នែក IT.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 29 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span>
+                                    ធ្លាប់តែឃើញគេត្រូ.លអ្នករៀន IT ដឹងអត់ថា អ្នកចេះជំនាញនេះច្បាស់ ចំណូ.ល១ខែប៉ុន្មាន?
+                                </span>
+                                <span>
+                                    អ្នកប្រហែលជាធ្លាប់បានឃើញ អំពីការបង្ហោះហូរហែលលើប្រព័ន្ធអ៊ីនធឺណេតជុំវិញពាក្យសម្តីលេងសើច ឬចំអកឡកលើយផ្សេងៗចំពោះក្រុមអ្នក IT។ ប៉ុន្តែតើដឹងទេថា អ្នកធ្វើការជំនាញផ្នែក IT អាចរកចំណូលបានប៉ុន្មានក្នុងមួយខែ?
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/Digital Cambodia/ធំអីធំយ៉ាងនេះ! Red Magic 10 Series.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 30 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span>
+                                    ធំអីធំយ៉ាងនេះ! Red Magic 10 Series នឹងញាត់ថ្មទំហំ 7,000mAh ក៏ដូចជាឈីបចុងក្រោយរបស់ Qualcomm ផងដែរ។
+                                </span>
+                                <span>
+                                    ទីបំផុត Red Magic 10 Series នឹងធ្វើការបញ្ចេញកំពូលស្មាតហ្វូនរបស់ខ្លួនជាផ្លូវការហើយ នៅថ្ងៃទី 13 ខែវិច្ឆិការ ខាងមុខ ហើយអ្វីដែលកាន់តែពិសេសសម្រាប់ Red Magi 10 series នេះគឺនឹងបំពាក់មកជាមួយឈីប ក៏ដូជាទំហំធំកប់មិនដែលមានថែមទៀតផង។
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+                <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" ata-aos-once="true">
+                    <div class="bg-content">
+                        <div class="img-content">
+                            <img src="img/Digital Cambodia/កុំឱ្យ CapCut ឡើងចាងពេក.jpg" alt="">
+                        </div>
+                        <div class="manage-title-content">
+                            <div class="date-content">
+                                <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 31 01 2025</span>
+                            </div>
+                            <div class="title-content">
+                                <span>
+                                    កុំឱ្យ CapCut ឡើងចាងពេក! Instagram ប្រកាសត្រៀមចេញកម្មវិធីកាត់វីដេអូថ្មីប្រជែងនឹង CapCut
+                                </span>
+                                <span>
+                                    Instagram កំពុងបង្កើនការប្រកួតប្រជែងក្នុងការបង្កើតវីដេអូ ជាមួយនឹងកម្មវិធីថ្មីមួយដែលមានឈ្មោះថា Edits ដែលជាឧបករណ៍កែវីដេអូឯករាជ្យមួយ សំដៅផ្តល់ឱ្យអ្នកបង្កើតនូវ "ឈុតឧបករណ៍ច្នៃប្រឌិតពេញលេញ" នេះបើយោងតាមប្រធាន Instagram លោក Adam Mosseri ។ នៅក្នុងវីដេអូដែលបានបង្ហោះនៅលើ Threads កាលពីថ្ងៃអាទិត្យ លោក Mosseri បានរៀបរាប់លម្អិតអំពីលក្ខណៈពិសេសរបស់កម្មវិធី ដោយដាក់វាជាគូប្រជែងដ៏ខ្លាំងក្លានៅក្នុងទីផ្សារកែវីដេអូ។
+                                </span>
+                            </div>
+                            <div class="user-post">
+                                <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                                <a href="https://t.me/lina_167" target="_blank">
+                                    <p><i class="bi bi-telegram"></i></p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="#" style="color: #ffffff; text-decoration: none;">
+            <div class="box-content col-12 col-md-6 col-sm-12 col-lg-4 col-xl-3" data-aos="fade-up" data-aos-once="true">
+                <div class="bg-content">
+                    <div class="img-content">
+                        <img src="img/Digital Cambodia/សារព័ត៌មានជប៉ុន NikkeiAsia.jpg" alt="">
+                    </div>
+                    <div class="manage-title-content">
+                        <div class="date-content">
+                            <span><i class="bi bi-calendar3" style="margin-right: 3px;"></i> 30 01 2025</span>
+                            <span><i class="bi bi-three-dots-vertical"></i></span>
+                        </div>
+                        <div class="title-content">
+                            <span>
+                                សារព័ត៌មានជប៉ុន NikkeiAsia: កម្ពុជាបិទវេបសាយជួញដូរគ្រីបតូចំនួន ១៦ លែងឱ្យចូលបាន
+                            </span>
+                            <span>
+                                យោងតាមអត្ថបទរបស់សារ​ព័ត៌មានជប៉ុន Nikkei Asia បានចេញផ្សាយថា កម្ពុជាបានបិទមិនឱ្យអ្នកប្រើប្រាស់ចូលបាននូវវេបសាយជួញដូរគ្រីបតូចំនួន ១៦ វេបសាយ រួមទាំងវេបសាយផ្សារជួញដូរគ្រីបតូដ៏ធំបំផុតលើពិភពលោក Binance ផងដែរ។
+                            </span>
+                        </div>
+                        <div class="user-post">
+                            <p><i bi class="bi bi-person-circle"></i>Post By: Voeurn Lina</p>
+                            <a href="https://t.me/lina_167" target="_blank">
+                                <p><i class="bi bi-telegram"></i></p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </a>
+        </div>
+    </div>
+</div>
+<?php include 'My PHP/footer.php'; ?>
+
+
+
+<!-- JavaScript -->
+<script src="javascript/scroll navbar.js"></script>
+<script src="javascript/Mouse hover on bg-content.js"></script>
+<script src="javascript/show list share.js"></script>
+<!-- Include AOS JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+<script>
+    AOS.init({
+        duration: 1500, // Applies to all animations
+        once: true
+    });
+</script>
+
+
+
+
+
+
+<body>
+
+</body>
+
+</html>
